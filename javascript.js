404e863b857b3aa5d03b65663a71f6354a159077
Jerry Yang <104951841+rebirth112@users.noreply.github.com>
2022-07-28 16:44:53 -0700

    Simplified UI for better UX

--- javascript.js
+++ javascript.js
@@ -7,24 +7,16 @@ let listComm = document.getElementById("lctotal");
 let purchasePrice = document.getElementById("pprice");
 let firstOne = document.getElementById("firstonehundred");
 let grossPercent = document.getElementById("grosspercentage");
-let additionalBonus = document.getElementById("additionalbonus");
-let deductions = document.getElementById("minusdeductions");
 let buyerOne = document.getElementById("buyeronehundred");
 let buyerPercent = document.getElementById("buyerpercentage");
-let buyerAdditional = document.getElementById("buyeradditionalbonus");
-let buyerDeduct = document.getElementById("buyerminusdeductions")
 
 
 resetButton.addEventListener('click', function(){
     purchasePrice.value = '';
     firstOne.value = '';
     grossPercent.value = '';
-    additionalBonus.value = '';
-    deductions.value = '';
     buyerOne.value = '';
     buyerPercent.value = '';
-    buyerAdditional.value = '';
-    buyerDeduct.value = '';
     listComm.innerHTML = '';
     sellComm.innerHTML = '';
     grossComm.innerHTML = '';
@@ -32,16 +24,16 @@ resetButton.addEventListener('click', function(){
 
 calcButton.addEventListener('click', function(){
     if (purchasePrice.value >= 100000) {
-        let finalGross = (100000 * (firstOne.value * 0.01)) + ((purchasePrice.value-100000) * (grossPercent.value * 0.01)) + additionalBonus.value - deductions.value;
+        let finalGross = (100000 * (firstOne.value * 0.01)) + ((purchasePrice.value-100000) * (grossPercent.value * 0.01))
         grossComm.innerHTML = Math.round((finalGross + Number.EPSILON) * 100)/100
-        let finalBuyer = (100000 * (buyerOne.value * 0.01)) + ((purchasePrice.value-100000) * (buyerPercent.value * 0.01)) + buyerAdditional.value - buyerDeduct.value;
+        let finalBuyer = (100000 * (buyerOne.value * 0.01)) + ((purchasePrice.value-100000) * (buyerPercent.value * 0.01))
         sellComm.innerHTML = Math.round((finalBuyer + Number.EPSILON) * 100)/100
         roundedList = finalGross - finalBuyer;
         listComm.innerHTML = Math.round((roundedList + Number.EPSILON) * 100)/100
     } else if (purchasePrice.value < 100000) {
-        let finalGross = (purchasePrice.value * (firstOne.value * 0.01)) + additionalBonus.value - deductions.value;
+        let finalGross = (purchasePrice.value * (firstOne.value * 0.01));
         grossComm.innerHTML = Math.round((finalGross + Number.EPSILON) * 100)/100
-        let finalBuyer = (purchasePrice.value * (buyerOne.value * 0.01)) + buyerAdditional.value - buyerDeduct.value;
+        let finalBuyer = (purchasePrice.value * (buyerOne.value * 0.01))
         sellComm.innerHTML = Math.round((finalBuyer + Number.EPSILON) * 100)/100
         roundedList = finalGross - finalBuyer;
         listComm.innerHTML = Math.round((roundedList + Number.EPSILON) * 100)/100
